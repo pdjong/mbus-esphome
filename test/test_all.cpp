61254c148ac0ecfb2b60ee7747739ca313a06d92
Paul de Jong <paul.m.de.jong@gmail.com>
2023-08-28 16:12:54 +0200

    First true tests for calculate_checksum

--- test/test_all.cpp
+++ test/test_all.cpp
@@ -1,37 +1,50 @@
 #include <Arduino.h>
 #include <test_includes.h>
-#include <bollocks.h>
 #include <TestableDataLinkLayer.h>
 #include <unity.h>
 
 void setUp(void) {}
 void tearDown(void) {}
 
-void test_bollocks_do_something_with_two_numbers(void) {
-  Bollocks bollocks;
-  TEST_ASSERT_EQUAL(0, bollocks.do_something_with_two_numbers(0, 0));
-  
-  TEST_ASSERT_EQUAL(2, bollocks.do_something_with_two_numbers(1, 0));
-  TEST_ASSERT_EQUAL(4, bollocks.do_something_with_two_numbers(2, 0));
-
-  TEST_ASSERT_EQUAL(1, bollocks.do_something_with_two_numbers(0, 1));
-  TEST_ASSERT_EQUAL(2, bollocks.do_something_with_two_numbers(0, 2));
-
-  TEST_ASSERT_EQUAL(3, bollocks.do_something_with_two_numbers(1, 1));
-  TEST_ASSERT_EQUAL(6, bollocks.do_something_with_two_numbers(2, 2));
-}
-
 void test_data_link_layer_calculate_checksum(void) {
   FakeUartInterface fakeUartInterface;
   TestableDataLinkLayer dataLinkLayer(&fakeUartInterface);
+  
+  uint8_t *data = new uint8_t[2] { 0, 0 };
+  TEST_ASSERT_EQUAL(0, dataLinkLayer.call_calculate_checksum(data, 2));
+  delete[] data;
 
-  uint8_t data[] { 0, 0 };
-  TEST_ASSERT_EQUAL(0, dataLinkLayer.call_calculate_checksum(0, 0));
+  data = new uint8_t[2] { 0, 1 };
+  TEST_ASSERT_EQUAL(1, dataLinkLayer.call_calculate_checksum(data, 2));
+  delete[] data;
+  data = new uint8_t[2] { 1, 0 };
+  TEST_ASSERT_EQUAL(1, dataLinkLayer.call_calculate_checksum(data, 2));
+  delete[] data;
+  
+  data = new uint8_t[2] { 1, 1 };
+  TEST_ASSERT_EQUAL(2, dataLinkLayer.call_calculate_checksum(data, 2));
+  delete[] data;
+  
+  data = new uint8_t[2] { 0, 255 };
+  TEST_ASSERT_EQUAL(255, dataLinkLayer.call_calculate_checksum(data, 2));
+  delete[] data;
+  data = new uint8_t[2] { 1, 255 };
+  TEST_ASSERT_EQUAL(0, dataLinkLayer.call_calculate_checksum(data, 2));
+  delete[] data;
+  data = new uint8_t[2] { 255, 1 };
+  TEST_ASSERT_EQUAL(0, dataLinkLayer.call_calculate_checksum(data, 2));
+  delete[] data;
+  data = new uint8_t[2] { 255, 2 };
+  TEST_ASSERT_EQUAL(1, dataLinkLayer.call_calculate_checksum(data, 2));
+  delete[] data;
+
+  data = new uint8_t[5] { 8, 128, 200, 0, 12 };
+  TEST_ASSERT_EQUAL(92, dataLinkLayer.call_calculate_checksum(data, 5));
+  delete[] data;
 }
 
 int runUnityTests(void) {
   UNITY_BEGIN();
-  RUN_TEST(test_bollocks_do_something_with_two_numbers);
   RUN_TEST(test_data_link_layer_calculate_checksum);
   return UNITY_END();
 }
