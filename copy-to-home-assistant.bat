c68179e87c235a1dd9827f7e5ae10ca0956568d8
Paul de Jong <paul.m.de.jong@gmail.com>
2023-09-10 09:40:12 +0200

    Add files to copy batch script

--- copy-to-home-assistant.bat
+++ copy-to-home-assistant.bat
@@ -10,11 +10,15 @@ set SrcDir=.\lib\heatmeter_mbus
 set customComponentsTargetDir=%targetDir%\custom_components\heatmeter_mbus
 
 copy %SrcDir%\__init__.py %customComponentsTargetDir%
+copy %SrcDir%\DataBlockReader.h %customComponentsTargetDir%
+copy %SrcDir%\DataBlockReader.cpp %customComponentsTargetDir%
 copy %SrcDir%\EspArduinoUartInterface.h %customComponentsTargetDir%
 copy %SrcDir%\HeatMeterMbus.cpp %customComponentsTargetDir%
 copy %SrcDir%\HeatMeterMbus.h %customComponentsTargetDir%
 copy %SrcDir%\Kamstrup303WA02.cpp %customComponentsTargetDir%
 copy %SrcDir%\Kamstrup303WA02.h %customComponentsTargetDir%
+copy %SrcDir%\MbusSensor.cpp %customComponentsTargetDir%
+copy %SrcDir%\MbusSensor.h %customComponentsTargetDir%
 copy %SrcDir%\Pwm.cpp %customComponentsTargetDir%
 copy %SrcDir%\Pwm.h %customComponentsTargetDir%
 copy %SrcDir%\sensor.py %customComponentsTargetDir%
